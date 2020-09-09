89c418dff26e62d076e9ef9086a12112d0acb33f
VictorXRivera <victorxrivera.professional@gmail.com>
2020-09-09 18:41:39 +0000

    Added method that gets number of each object by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,20 @@ def json_status():
     Method to return a JSON
     """
     return jsonify({"status": OK})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def endpoint():
+    """
+    Method that retrieves the number of each objects by type
+    """
+    status = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+
+    return jsonify(status)
